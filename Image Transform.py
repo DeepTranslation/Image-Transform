66238c06ebda5cf04de884a90eea671a0761b766
DeepTranslation <anne261@gmx.de>
2020-05-02 00:13:29 +0200

    transformation functions defined

--- Image Transform.py
+++ Image Transform.py
@@ -1,57 +1,113 @@
-
 # coding: utf-8
-
 # Matrix transformations on images
 
-# In[14]:
 
-
-import math, sympy, numpy
-
-import matplotlib.image as mpimg
-
+import sys
+import argparse
+import math
 import numpy as np
 import matplotlib.pyplot as plt
-import PIL
 from PIL import Image
 
+class CustomAction(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        if not 'ordered_args' in namespace:
+            setattr(namespace, 'ordered_args', [])
+        previous = namespace.ordered_args
+        previous.append((self.dest, values))
+        setattr(namespace, 'ordered_args', previous)
+parser = argparse.ArgumentParser()
+#parser.add_argument("-h", "--help", help="Linear transformation of images.")
 
-# In[8]:
+parser.add_argument("-i", "--inputfile", help="Image file to be transformed")
+parser.add_argument("-o", "--outputfile", help="name of the outputfile")
+parser.add_argument("-sc", "--scale",  type=float, help="scales the image by the factor", action=CustomAction)
+#parser.add_argument("-sh", "--scale", type=float, help="scales the image by the factor")
+parser.add_argument("-r", "--rotate", type=float, help="Rotates the image by angle", action=CustomAction)
+parser.add_argument("-m", "--mirror", type=float, help="Mirrors along the mirror axis defined by its angle", action=CustomAction)
 
 
-# set display Options for jupyter notebook
-np.set_printoptions(linewidth=180)
 
 
-# In[17]:
+args = parser.parse_args()
+
+print(args.inputfile)
+print(args.outputfile)
+print(args.ordered_args)
+
 
 
 # Open image file as numpy array
-file = "BellenA2.jpg"
-img = Image.open(file)
+
+
+try:
+    img = Image.open(args.inputfile, 'r')
+except IOError as e:
+    print('cannot open', args.inputfile)
+    print( "I/O error({0}): {1}".format(e.errno, e.strerror))
+    sys.exit(1)
+#img = Image.open(FILE)
 imageArray = np.asarray(img)
 imageWidth=imageArray.shape[1]
 imageHeight=imageArray.shape[0]
 print(imageArray.shape)
 
 
-# In[16]:
 
 
 # Plot image array
-plt.figure()
-plt.imshow(imageArray)
-plt.show()
+#plt.figure()
+#plt.imshow(imageArray)
+#plt.show()
+
+trfMatrix=np.array([[1,0],[0,1]])
+
+def horizontalShear (s):
+    # horizontal shear (-2<=s<=2)
+    return np.array([[1,0],[s,1]])
+
+def verticalShear (s):
+    # vertical shear (-2<=s<=2)
+    return np.array([[1,s],[0,1]])
+
+def horizontalScaling (h):
+    # horizontal scaling
+    # absolute values > 1 =  decreasing size
+    # absolute values between 0 and 1 =  increasing size
+    # values below 0 = mirroring
+    return np.array([[1,0],[0,h]])
+
+def verticalScaling (v):
+    #  vertical scaling
+    # absolute values > 1 =  decreasing size
+    # absolute values between 0 and 1 =  increasing size
+    # values below 0 = mirroring
+    return np.array([[v,0],[0,1]])
+
+def rotation (angle):
+    #  rotation
+    # angle in degrees
+    angle=math.radians(angle)
+    return np.array([[math.cos(alpha),math.sin(alpha)],[-math.sin(alpha),math.cos(alpha)]])
 
 
-# In[204]:
+def mirroring (angle):
+    #  mirroring
+    # angle in degrees
+    angle=math.radians(angle)
+    return np.array([[math.cos(alpha),math.sin(alpha)],[math.sin(alpha),-math.cos(alpha)]])
 
+
+print(args.ordered_args[0][1])
+trfMatrix=trfMatrix.dot(shear(args.ordered_args[0][1]))
+#trfMatrix*=shear(args.ordered_args[0][1])
+
+'''
 
 # show new coordinates, if so desired
 # newcoordinateArray
 
 
-# In[267]:
 
 
 #create transformation matrix
@@ -76,12 +132,10 @@ trfMatrix=np.array([[v,0],[0,h]])
 alpha=math.pi/5
 trfMatrix=np.array([[math.cos(alpha),math.sin(alpha)],[-math.sin(alpha),math.cos(alpha)]])
 
+'''
 # mirroring
-alpha = math.pi/3
-trfMatrix=np.array([[math.cos(alpha),math.sin(alpha)],[math.sin(alpha),-math.cos(alpha)]])
-
-
-# In[268]:
+#alpha = math.pi/3
+#trfMatrix=np.array([[math.cos(alpha),math.sin(alpha)],[math.sin(alpha),-math.cos(alpha)]])
 
 
 # make array with new coordinates by multiplying 
@@ -91,9 +145,6 @@ newcoordinateArray=np.asarray([[trfMatrix.dot([y,x])]
                                for y in range(-round(imageHeight/2),round(imageHeight/2))
                                for x in range(-round(imageWidth/2),round(imageWidth/2))]).reshape(imageHeight,imageWidth,2)
 newcoordinateArray.shape
-
-
-# In[269]:
 
 
 # Transform image by storing the pixel values for each pixel in the new image
@@ -106,15 +157,10 @@ transformedImage=np.asarray([imageArray[int(round(newcoordinateArray[y,x,0]+imag
                              else imageArray[0,0]  for y in range(imageHeight)for x in range(imageWidth)])
 
 
-# In[205]:
-
 
 # show shapes of the transformed image array, if so desired
 # transformedImage.shape
 # transformedImage.reshape(imageHeight,imageWidth,3).shape
-
-
-# In[270]:
 
 
 # Plot image array
@@ -123,10 +169,33 @@ plt.imshow(transformedImage.reshape(imageHeight,imageWidth,3))
 plt.show()
 
 
-# In[271]:
-
-
 # Save Image
 img= Image.fromarray(transformedImage.reshape(imageHeight,imageWidth,3))
-img.save("Transform1.png")
+img.save(args.outputfile)
+sys.exit(1)
 
+'''
+
+def main(argv):
+   inputfile = ''
+   outputfile = ''
+   try:
+      opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+   except getopt.GetoptError:
+      print 'test.py -i <inputfile> -o <outputfile>'
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print 'test.py -i <inputfile> -o <outputfile>'
+         sys.exit()
+      elif opt in ("-i", "--ifile"):
+         inputfile = arg
+      elif opt in ("-o", "--ofile"):
+         outputfile = arg
+   print 'Input file is "', inputfile
+   print 'Output file is "', outputfile
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
+
+   '''
